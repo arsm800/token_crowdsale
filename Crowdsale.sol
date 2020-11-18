3e2c638cd4672ed5be5ce5796ae0ec82e1d57088
Andrew Smith-Mui <asmithmui@gmail.com>
2020-11-18 17:30:55 -0500

    Create PupperCoin token in 'PupperCoinSaleDeployer' contract.

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -12,12 +12,11 @@ contract PupperCoinSale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCro
 
     constructor(
         // Fill in the constructor parameters!
-        string memory name,
-        string memory symbol,
+        uint rate,
         address payable wallet,
         PupperCoin token,
         uint goal,
-        uint rate,
+        uint cap,
         uint openingTime,
         uint closingTime
     )
@@ -43,14 +42,22 @@ contract PupperCoinSaleDeployer {
     address public token_address;
 
     constructor(
-        // @TODO: Fill in the constructor parameters!
+        //Fill in the constructor parameters
+        string memory name,
+        string memory symbol,
+        address payable wallet //will receive ETH raised by sale
+        
     )
         public
     {
-        // @TODO: create the PupperCoin and keep its address handy
-       
+        //create the PupperCoin and keep its address handy
+       PupperCoin token = new PupperCoin(name, symbol, 0);
+       token_address = address(token);
 
-        // @TODO: create the PupperCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
+        //create the PupperCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
+        PupperCoinSale PupperCoin_sale = new PupperCoinSale(1, wallet, token);
+        //How does it know to expect 7 arguments?
+        token_sale_address = address(PupperCoin_sale);
 
         // make the PupperCoinSale contract a minter, then have the PupperCoinSaleDeployer renounce its minter role
         token.addMinter(token_sale_address);
